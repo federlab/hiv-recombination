61df36b3994b015cec98a9fd03e9a7d64055bde5
Elena Romero <evromero@grid-head2.grid.gs.washington.edu>
2021-04-03 18:16:28 -0700

    started sucessfully sorting the fastqs by patient

--- data/strauli/sortFastqs.py
+++ data/strauli/sortFastqs.py
@@ -11,37 +11,43 @@ infoFile = "SraRunInfo.csv"
 #open the csv with all of the data
 runInfo = pd.read_csv(infoFile)
 
-print(runInfo, file=sys.stderr )
 
 #iterate through the fastq files in the directory
 for filename in os.listdir(dirPath):
-    #just get the first version (I accidentally got duplicates of the files)
-    version = filename.split('_')
-    accession = version[0]
-    version = version[1].split('.')
-    version = int(version[0])
-    if version == 1:
+    if not os.isdir(filename):
+        #just get the first version (I accidentally got duplicates of the files)
         print(filename, file = sys.stderr)
+        version = filename.split('_')
+        accession = version[0]
+        version = version[1].split('.')
+        version = int(version[0])
+        if version == 1:
+            
 
-        #now we need to determine if the file is env sequences
-        currRow = runInfo.loc[runInfo['Run'] == accession]
-        #check if the file is viral RNA
-        isRNA = (currRow['LibrarySource'] == 'VIRAL RNA')
-        if isRNA:
-            patient = currRow['SampleName']
-            currLibrary = currRow['LibraryName']
-            print(currLibrary, file = sys.stderr)
-        
-            #make sure to make the patient directory
-            if not os.path.exists(patient):
-                os.makedir(patient)
-                
-            #rename and move the file 
-            newName = filename.split('.')
-            newName = newName[0] + currLibrary + ".fastq"
-            print(newName, file=sys.stderr)
-            os.rename(dirPath + filename, dirPath + patient + "/" + newName)
-        #move the antibody sequences to a different directory
-        else:
-            os.rename(dirPath + filename, dirPath + "transcriptome/" + newName)
-        
+            #now we need to determine if the file is env sequences
+            currRow = runInfo.loc[runInfo['Run'] == accession]
+            # print("the row with sequence info is", file = sys.stderr)
+            # print(currRow, file = sys.stderr)
+            #check if the file is viral RNA
+            isRNA = ((currRow['LibrarySource'] == 'VIRAL RNA').to_numpy())[0]
+            # print("isRNA is", file = sys.stderr)
+            # print(isRNA, file = sys.stderr)
+            if isRNA:
+                patient = ((currRow['SampleName']).to_numpy())[0]
+                currLibrary = ((currRow['LibraryName']).to_numpy())[0]
+                # print("the library is", file = sys.stderr)
+                # print(currLibrary, file = sys.stderr)
+            
+                #make sure to make the patient directory
+                if not os.path.exists(dirPath + patient):
+                    os.makedirs(dirPath + patient)
+                    
+                #rename and move the file 
+                newName = filename.split('.')
+                newName = newName[0] + "_" + currLibrary + ".fastq"
+                # print(newName, file=sys.stderr)
+                os.rename(dirPath + filename, dirPath + patient + "/" + newName)
+            #move the antibody sequences to a different directory
+            else:
+                os.rename(dirPath + filename, dirPath + "transcriptome/" + newName)
+            
