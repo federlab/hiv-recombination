62ff1f39c290c8a3f948ea7c8c49c0a6e7b71c90
Elena Romero <evromero@nexus2.gs.washington.edu>
2021-04-19 13:54:08 -0700

    started on simple r^2 calculations

---
+++ bin/r2Analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def calculateR2():
+    """
+    Given two segregating loci, calculate their R^2 value
+    https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0048588
+    """
+    #We need to know both alleles at each locus
+    #To get this we can use our vcf file.
+    
+    #I think I need to ask more about which files to use (should I use samples
+    # from multiple patient dates, or should there be a plot for each date)
+
+    #We need the allele frequencies from the intersecting reads
+
+    
