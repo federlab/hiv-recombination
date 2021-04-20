366a687c0a9a61b762121d7178683cb7d679957f
Elena Romero <evromero@nexus1.gs.washington.edu>
2021-04-19 17:07:31 -0700

    parsed vcf files

--- bin/r2Analysis.py
+++ bin/r2Analysis.py
@@ -1,4 +1,54 @@
+import pysam
+import allel
+import sys
 import numpy as np
+import pandas as pd
+
+
+def get_VCF_Loci(vcfFile):
+    """ Open a vcf file and save the variants in it to a dataframe 
+    ---------------------------------------------------------------------------
+    Params
+    -------
+    vcfFile     str, path to the vcf file 
+    Returns
+    -------
+    vcfDat   pd.dataframe, containing each variant for the current dataset
+    """
+    #load our data into a dataframe
+    vcfDat = allel.read_vcf(vcfFile)
+    #check for extra alternate alleles
+    if vcfDat['variants/ALT'].shape[1] > 3:
+        print('More than 3 alternate alleles', file = sys.stderr)
+
+    vcfDat = pd.DataFrame({'POS': vcfDat['variants/POS'], 
+            'REF': vcfDat['variants/REF'] ,
+             'ALT1': (vcfDat['variants/ALT'])[:,0], 
+             'ALT2': (vcfDat['variants/ALT'])[:,1], 
+             'ALT3': (vcfDat['variants/ALT'])[:,2]})
+
+    return vcfDAt
+
+def get_sam_reads(vcfDF, samFile):
+    """ Loop through a vcf file and get all possible pairs of loci that are
+    segregating.
+    ---------------------------------------------------------------------------
+    Params
+    ------
+    vcfDF      pd.dataframe, containing each variant for the current dataset
+    """
+    #we need to get each pair of loci. To do this we can iterate through the
+    #rows with nested for loops
+    for index1, locus1 in vcfDF.iterrows():
+        for index2, locus2 in vcfDF.iterrows():
+            #now we can use the two loci to get all of the reads between them
+            #we can do this using pysam
+            print(locus1, file = sys.stderr)
+            print(locus2, file = sys.stderr)
+
+    return
+
+
 
 def calculateR2():
     """
@@ -7,6 +57,7 @@ def calculateR2():
     """
     #We need to know both alleles at each locus
     #To get this we can use our vcf file.
+
     
     #I think I need to ask more about which files to use (should I use samples
     # from multiple patient dates, or should there be a plot for each date)
